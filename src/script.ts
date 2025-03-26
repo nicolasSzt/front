2ebbeaa234cf27fad3578d762c33f295de17c199
Nicolas Steinjamer <nsteinjamer.ext@ticmas.com>
2025-03-26 12:13:55 -0300

    chore: create proyect

---
+++ src/script.ts
@@ -0,0 +1,22 @@
+class Veiculo {
+    marca:string
+    color:string
+    distancia_recorrida
+
+    constructor(marca: string, color: string, distancia_recorrida: number) {
+        this.marca = marca;
+        this.color = color;
+        this.distancia_recorrida = distancia_recorrida;
+
+    }
+
+    aumentarDistancia(distancia: number) {
+        this.distancia_recorrida = this.distancia_recorrida += distancia
+    }
+}
+
+const autoRojo = new Veiculo("Ferrari", "Rojo", 10000)
+
+autoRojo.aumentarDistancia(90)
+
+console.log(autoRojo);
