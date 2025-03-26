c8fe892974cb22c5b63d511f12c065351d126dcc
Nicolas Sztajnchamer <nicostein2134@gmail.com>
2025-03-26 12:29:09 -0300

    chore: adjust lint

--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,26 @@
 class Veiculo {
-    marca:string
-    color:string
-    distancia_recorrida
+    marca: string
+    color: string
+    distancia_recorrida:number
 
-    constructor(marca: string, color: string, distancia_recorrida: number) {
+    constructor(
+        marca: string,
+        color: string,
+        distancia_recorrida: number
+    ) {
         this.marca = marca;
         this.color = color;
         this.distancia_recorrida = distancia_recorrida;
-
     }
 
     aumentarDistancia(distancia: number) {
         this.distancia_recorrida = this.distancia_recorrida += distancia
+        console.log(autoRojo);
     }
+
 }
 
 const autoRojo = new Veiculo("Ferrari", "Rojo", 10000)
 
 autoRojo.aumentarDistancia(90)
 
-console.log(autoRojo);
