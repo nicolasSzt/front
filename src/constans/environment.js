a29f32803ba7e6a4442d4140496b1add5f5a9582
Nicolas Steinjamer <nsteinjamer.ext@ticmas.com>
2025-05-29 16:08:15 -0300

    chore:remove unessesary

--- src/constans/environment.js
+++
@@ -1,5 +0,0 @@
-const ENVIRONMENT = {
-  apiUrl: import.meta.env.VITE_URL_API,
-};
-
-export default ENVIRONMENT;
--- src/constans/token.js
+++
@@ -1,5 +0,0 @@
-const LOCALSTORAGE_KEYS = {
-    AUTHORIZATION_TOKEN: 'authorization_token'
-}
-
-export default LOCALSTORAGE_KEYS
