8a3edcfc17bd6d3c7aeed6a01f9577ab48719438
Nicolas Steinjamer <nicostein2134@gmail.com>
2025-06-22 02:42:48 -0300

    chore: update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
